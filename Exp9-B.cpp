611eb5f204958f851b38efc7377c69b89c4a3b6c
Varija22 <varija.korti.btech2024@sitpune.edu.in>
2025-08-12 15:09:17 +0530

    Create Exp9-B.cpp

---
+++ Exp9-B.cpp
@@ -0,0 +1,18 @@
+//Varija K
+//24070123165
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 10;
+    int *aptr;
+    aptr = &a;
+
+    cout << a << endl;        // prints value of a
+    cout << aptr << endl;     // prints address stored in pointer (address of a)
+    cout << *aptr << endl;    // prints value at address stored in pointer (value of a)
+    cout << &a << endl;       // prints address of a
+
+    return 0;
+}
